2f41d6b532d59664a3b2368af2f983345e4b070b
Erik  Roche <erikroche@eriks-air.home>
2023-12-24 13:12:07 +0000

    Created two functions that you can pass a prompt to and if the prompt matches either an expression or the generation of an image, the AI will call the respective function and either use a calculator to reply or call to the DALL-E API to generate an image using their API

---
+++ function.js
@@ -0,0 +1,95 @@
+import { openai } from "./openai.js";
+import 'dotenv/config'
+import math from 'advanced-calculator'
+const QUESTION = process.argv[2] || "hi"
+
+const messages = [
+    {
+        role: "user",
+        content: QUESTION,
+    },
+]
+
+const functions = {
+    calculate({ expression }) {
+        return math.evaluate(expression)
+    },
+
+    async generateImage({ prompt }) {
+        const result = await openai.images.generate({prompt})
+        console.log(prompt)
+        console.log(result)
+        return result.data[0].url
+    },
+}
+
+const getCompletion = (message) => {
+    return openai.chat.completions.create({
+        model: "gpt-3.5-turbo-0613",
+        messages,
+        temperature: 0,
+        functions: [
+            {
+                name: "calculate",
+                description: "Run math expressions",
+                parameters: {
+                    type: "object",
+                    properties: {
+                        expression: {
+                            type: "string",
+                            description: "Then math expression to evaluate like '2 * 3 + (21 / 2) ^ 2'",
+                        },
+                    },
+                    required: ["expression"],
+                },
+            },
+            {
+                name: "generateImage",
+                description: "Create or generate image based on a description",
+                parameters: {
+                    type: "object",
+                    properties: {
+                        prompt: {
+                            type: "string",
+                            description: "The description of the image to generate",
+                        },
+                    },
+                    required: ["prompt"],
+                },
+            },
+        ],
+    })
+}
+
+let response
+while(true) {
+    response = await getCompletion(messages)
+
+    if (response.choices[0].finish_reason === "stop") {
+        console.log(response.choices[0].message.content)
+        break
+    } else if (response.choices[0].finish_reason === "function_call") {
+        const fName = response.choices[0].message.function_call.name
+        const args = response.choices[0].message.function_call.arguments
+
+        const funcToCall = await functions[fName]
+        const params = JSON.parse(args)
+
+        const result = funcToCall(params)
+
+        messages.push({
+            role: "assistant",
+            content: null,
+            function_call: {
+                name: fName,
+                arguments: args,
+            },
+        })
+
+        messages.push({
+            role: "function",
+            name: fName,
+            content: JSON.stringify({ result })
+        })
+    }
+}
